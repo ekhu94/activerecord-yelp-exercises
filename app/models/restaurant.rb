2cbd81cd17048977e6a32a7af7bda7320aef585d
Erik Huang <erikhuangdrums@gmail.com>
2021-02-02 10:42:19 -0800

    start methods for restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,21 @@
 class Restaurant < ActiveRecord::Base
     has_many :dishes
+
+    def self.mcdonalds
+        self.find_by(name: 'McDonalds')
+    end
+
+    def self.tenth
+        self.find(10)
+    end
+
+    # not working
+    def self.with_long_names
+        self.where('name.length > 12')
+    end
+
+    def max_dishes
+        
+    end
+
 end
