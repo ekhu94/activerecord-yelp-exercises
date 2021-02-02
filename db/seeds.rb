a2c4b966109a3d4a8e31b725a986f7f0c7e27e24
Adan Escamilla <adanescamilla580@gmail.com>
2021-02-02 10:57:41 -0800

    something

---
+++ db/seeds.rb
@@ -0,0 +1,26 @@
+Restaurant.destroy.all
+
+r1 = Restaurant.create(name: "BWW")
+r2 = Restaurant.create(name: "Red Robin")
+r3 = Restaurant.create(name: "East India Grill")
+r4 = Restaurant.create(name: "Red Lobster")
+r5 = Restaurant.create(name: "Pho Kim")
+r6 = Restaurant.create(name: "Trappers Sushi")
+r7 = Restaurant.create(name: "Saltys")
+r8 = Restaurant.create(name: "Asadero")
+r9 = Restaurant.create(name: "Joeys")
+r10 = Restaurant.create(name:"Maza Grill")
+r11 = Restaurant.create(name:"Steak House")
+r12 = Restaurant.create(name:"Texas de Brazil")
+r13 = Restaurant.create(name:"Olive Garden")
+r14 = Restaurant.create(name:"Al Lago")
+r15 = Restaurant.create(name:"Mediterranean Breeze")
+r16 = Restaurant.create(name:"Katsu Burger")
+r17 = Restaurant.create(name:"Ichi Teriyaki")
+r18 = Restaurant.create(name:"8ight")
+r19 = Restaurant.create(name:"Seoul Garden")
+r20 = Restaurant.create(name:"Asiana Teriyaki")
+
+
+
+d1 = Dish.create(name: "Pasta", restaurant_id: 1)
